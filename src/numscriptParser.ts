efe129a5c0d627975b9ca81526208b6eafc0d2b3
felipe lussi <felipe-p-l@hotmail.com>
2025-01-13 19:01:52 -0300

    feat: update transactionGenerator

---
+++ src/numscriptParser.ts
@@ -0,0 +1,18 @@
+// const payload: NumscriptTransaction = {
+//   send: transactions,
+//   txMeta: {
+//     type: 'V1_ACCOUNT_SEND',
+//     transactionTypeDescription: 'Envio de pontos',
+//     description: 'description',
+//   },
+// };
+//
+// const parsedPayload = generateNumscript(payload)
+// const transaction = {
+//   refreference: `v1:transaction:${faker.string.uuid()}`,
+//   timestamp: "2021-11-17T10:20:55Z",
+//   script: {
+//     plain: parsedPayload,
+//   },
+// }
+//
--- src/transactionGenerator.ts
+++ src/transactionGenerator.ts
@@ -1,6 +1,7 @@
 import { fakerPT_BR as faker } from '@faker-js/faker';
 import { NumscriptTransaction } from 'formance-numscript-generator/src/types';
 import { generateNumscript } from 'formance-numscript-generator/src';
+import { Transaction, TransactionType } from './types';
 
 function main() {
 
@@ -8,37 +9,17 @@ function main() {
   const companies: string[] = [];
 
 
-  // const destination = `${faker.string.uuid()}:credit:${faker.string.uuid()}`
   for (let j = 1; j <= 10; j++) {
-    const destination = faker.string.uuid();
-    transactions.push({
-      asset: 'BRL/2',
+    const transaction: Transaction = {
+      id: faker.number.int(),
+      transactionType: 'V2_ACCOUNT_SEND',
+      reference: `v2:transaction:${faker.string.uuid()}`,
+      description: faker.finance.transactionType(),
+      destination: faker.string.uuid(),
       amount: 10000000,
-      sources: [{ account: 'world' }],
-      destinations: [{ account: destination }],
-    });
-    companies.push(destination);
+      source: 'world'
+    }
+    transactions.push(transaction);
+    companies.push(transaction.destination)
   }
-
-
-
-  const payload: NumscriptTransaction = {
-    send: transactions,
-    txMeta: {
-      type: 'V2_ACCOUNT_SEND',
-      transactionTypeDescription: 'Envio de pontos',
-      description: 'description',
-    },
-  };
-
-  const parsedPayload = generateNumscript(payload)
-  const transaction = {
-    refreference: `v2:transaction:${faker.string.uuid()}`,
-    timestamp: "2022-11-17T10:20:55Z",
-    script: {
-      plain: parsedPayload,
-    },
-  }
-
-
 }
