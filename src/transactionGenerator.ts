74816b208695f57a514dff9386bee21e5976c349
felipe lussi <felipe-p-l@hotmail.com>
2025-01-13 18:35:22 -0300

    feat: update transactionGenerator

--- src/transactionGenerator.ts
+++ src/transactionGenerator.ts
@@ -1,34 +1,44 @@
 import { fakerPT_BR as faker } from '@faker-js/faker';
 import { NumscriptTransaction } from 'formance-numscript-generator/src/types';
+import { generateNumscript } from 'formance-numscript-generator/src';
 
 function main() {
 
   const transactions = [];
+  const companies: string[] = [];
 
-  const destination = `${faker.string.uuid()}:credit:${faker.string.uuid()}`
+
+  // const destination = `${faker.string.uuid()}:credit:${faker.string.uuid()}`
   for (let j = 1; j <= 10; j++) {
+    const destination = faker.string.uuid();
     transactions.push({
-      asset: "BRL/2",
-      amount: faker.number.int({max: 1000}),
-      sources: [{account: "world"}],
-      destinations: [{account: destination}],
-    })
+      asset: 'BRL/2',
+      amount: 10000000,
+      sources: [{ account: 'world' }],
+      destinations: [{ account: destination }],
+    });
+    companies.push(destination);
   }
+
+
+
   const payload: NumscriptTransaction = {
     send: transactions,
     txMeta: {
       type: 'V2_ACCOUNT_SEND',
       transactionTypeDescription: 'Envio de pontos',
-      description: "description",
+      description: 'description',
     },
-    accountMeta: {
-      [destination]: {
-        expirationDate: "2025-11-17T10:20:55Z",
-      },
+  };
+
+  const parsedPayload = generateNumscript(payload)
+  const transaction = {
+    refreference: `v2:transaction:${faker.string.uuid()}`,
+    timestamp: "2022-11-17T10:20:55Z",
+    script: {
+      plain: parsedPayload,
     },
   }
 
 
-
 }
-
